bdccc56ec7bd7add7fccf72dfb46631358fe3caa
ajnanmvr <ajnanpulikkathody@gmail.com>
2026-03-21 00:00:56 +0530

    Add initial HTML structure, CSS styles, and JavaScript functionality for the Gushwork assignment

---
+++ script.js
@@ -0,0 +1,19 @@
+// ================= STICKY HEADER =================
+function handleStickyHeader() {}
+
+// ================= CAROUSEL =================
+function initCarousel() {}
+
+// ================= IMAGE ZOOM =================
+function handleImageZoom() {}
+
+// ================= FAQ TOGGLE =================
+function handleFAQ() {}
+
+// ================= INIT =================
+document.addEventListener("DOMContentLoaded", () => {
+  handleStickyHeader();
+  initCarousel();
+  handleImageZoom();
+  handleFAQ();
+});
